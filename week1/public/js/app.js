e71235238b7bad953244d3a8ee77ee5d817c6212
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-02-04 14:29:04 +0100

    GET data from rijksmuseum API, parsed to JSON

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,1 +1,13 @@
 console.log("lets go");
+
+var request = new XMLHttpRequest();
+var url =
+  "https://www.rijksmuseum.nl/api/nl/collection?key=GJXUiTlF&format=json&type=schilderij&ps=100&f.normalized32Colors.hex=%20%23737C84&imgonly=true";
+request.addEventListener("load", getData);
+request.open("GET", url, true);
+request.send();
+
+function getData() {
+  var data = JSON.parse(this.response);
+  console.log(data);
+}
