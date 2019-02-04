11a975f8af873199497fe43b330c22106e5ed6f0
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-02-04 16:04:22 +0100

    show specific data in html

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -7,7 +7,28 @@ request.addEventListener("load", getData);
 request.open("GET", url, true);
 request.send();
 
+var body = document.querySelector("body");
+
 function getData() {
   var data = JSON.parse(this.response);
   console.log(data);
+  var artObjects = data.artObjects;
+  console.log(artObjects);
+  artObjects.forEach(function(data) {
+    var p = document.createElement("p");
+    var img = document.createElement("img");
+    var longTitle = document.createTextNode(data.longTitle);
+    var imgUrl = document;
+    // var image = data.webImage.url;
+    var imgUrl = img.src;
+    imgUrl = data.webImage.url;
+    console.log(imgUrl);
+
+    img.setAttribute("src", imgUrl);
+
+    body.append(p);
+    body.append(img);
+    p.append(longTitle);
+    img.append(imgUrl);
+  });
 }
