e9b9277341df318bdc55060fcc414e2a82f829e1
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-02-06 12:50:03 +0100

    added GET request with a promise

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,34 +1,81 @@
+"use strict";
 console.log("lets go");
 
-var request = new XMLHttpRequest();
+// XMLHttpRequest (XHR)
+// var body = document.querySelector("body");
+// var request = new XMLHttpRequest();
+// var url =
+//   "https://www.rijksmuseum.nl/api/nl/collection?key=GJXUiTlF&format=json&type=schilderij&ps=100&f.normalized32Colors.hex=%20%23737C84&imgonly=true";
+//
+// request.open("GET", url, true);
+// request.onload = function() {
+// var data = JSON.parse(this.response);
+// var filteredData = filterData(data);
+// var content = renderContent(filteredData);
+// };
+// request.send();
+//
+// function getData(data) {
+//   var data = JSON.parse(this.response);
+//   var filteredData = filterData(data);
+//   var content = renderContent(filteredData);
+// }
+//
+// function filterData(data) {
+//   var filteredData = data.artObjects;
+//   return filteredData;
+// }
+//
+// function renderContent(data) {
+//   data.forEach(function(data) {
+//     var p = document.createElement("p");
+//     var img = document.createElement("img");
+//     var longTitle = document.createTextNode(data.longTitle);
+//     var imgUrl = data.webImage.url;
+//
+//     img.setAttribute("src", imgUrl);
+//     body.append(img, p);
+//     img.append(imgUrl);
+//     p.append(longTitle);
+//   });
+// }
+
+// Promises
+var body = document.querySelector("body");
 var url =
   "https://www.rijksmuseum.nl/api/nl/collection?key=GJXUiTlF&format=json&type=schilderij&ps=100&f.normalized32Colors.hex=%20%23737C84&imgonly=true";
-request.addEventListener("load", getData);
-request.open("GET", url, true);
-request.send();
 
-var body = document.querySelector("body");
+var loadData = new Promise(function(resolve, reject) {
+  var request = new XMLHttpRequest();
+  console.log(resolve);
+  request.open("GET", url, true);
+  request.onload = function() {
+    var data = JSON.parse(this.response);
+    resolve(data);
+  };
+  request.send();
+});
 
-function getData() {
-  var data = JSON.parse(this.response);
-  console.log(data);
-  var artObjects = data.artObjects;
-  console.log(artObjects);
-  artObjects.forEach(function(data) {
+loadData.then(data => {
+  var filteredData = getFilteredData(data);
+  var elements = createElements(filteredData);
+});
+
+function getFilteredData(data) {
+  var filteredData = data.artObjects;
+  return filteredData;
+}
+
+function createElements(filteredData) {
+  filteredData.forEach(function(data) {
     var p = document.createElement("p");
     var img = document.createElement("img");
     var longTitle = document.createTextNode(data.longTitle);
-    var imgUrl = document;
-    // var image = data.webImage.url;
-    var imgUrl = img.src;
-    imgUrl = data.webImage.url;
-    console.log(imgUrl);
+    var imgUrl = data.webImage.url;
 
     img.setAttribute("src", imgUrl);
-
-    body.append(p);
-    body.append(img);
-    p.append(longTitle);
+    body.append(img, p);
     img.append(imgUrl);
+    p.append(longTitle);
   });
 }
